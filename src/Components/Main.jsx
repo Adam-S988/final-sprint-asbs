79775146010a14d09480bbaafa8ec262917de52a
Adam-S988 <adam.stevenson@keyin.com>
2024-08-05 13:28:53 -0230

    Another push

--- src/Components/Main.jsx
+++ src/Components/Main.jsx
@@ -1,16 +1,37 @@
 import React from "react";
+import Main1 from "../Images/Main1.jpg";
+import Main2 from "../Images/Main2.jpg";
+import { Link } from "react-router-dom";
 
 const Main = () => {
   return (
     <div className="MainContainer">
-      <div className="MainBox">
-        <div className="innerBox">
+      <div
+        className="MainBox"
+        style={{
+          backgroundImage: `url(${Main1})`,
+          backgroundSize: "cover",
+          backgroundPosition: "center",
+          height: "500px",
+          width: "100%",
+        }}
+      >
+        <Link to="/productlist" className="innerBox">
           <p>Shop Home Goods</p>
-        </div>
+        </Link>
       </div>
-      <div className="MainBox">
+      <div
+        className="MainBox"
+        style={{
+          backgroundImage: `url(${Main2})`,
+          backgroundSize: "cover",
+          backgroundPosition: "center",
+          height: "500px",
+          width: "100%",
+        }}
+      >
         <div className="innerBox">
-          <p>Subscribe to our Newsletter for 40% off your first order</p>
+          <p>Subscribe to our Newsletter for 40% off your first order!</p>
         </div>
       </div>
     </div>
