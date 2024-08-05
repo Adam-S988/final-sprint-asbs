8cb69b19ec52a567facb43c64aae8fd4993d58db
Adam-S988 <adam.stevenson@keyin.com>
2024-08-05 11:56:10 -0230

    Small Push

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -7,19 +7,6 @@ const Header = () => {
   return (
     <div className="Header">
       <h1 className="Title">Normal Company.</h1>
-      {/* <Router>
-        <div className="container">
-          <Link to="/">Home</Link>
-          <Link to="/productlist">Shop</Link>
-          <Link to="/productlist">About Us</Link>
-          <Link to="/productlist">Contact</Link>
-          <Link to="/shoppingcart">Cart</Link>
-          <Routes>
-            <Route path="/productlist" element={<ProductList />} />
-            <Route path="/shoppingcart" element={<ShoppingCart />} />
-          </Routes>
-        </div>
-      </Router> */}
     </div>
   );
 };
