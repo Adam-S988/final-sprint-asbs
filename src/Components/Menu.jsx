74dcd642fd1973bd0bd4ff483aeff75655d944c3
Brandon Shea <brandon.shea1994@icloud.com>
2024-08-06 14:14:54 -0230

    Menu changes

--- src/Components/Menu.jsx
+++ src/Components/Menu.jsx
@@ -2,9 +2,22 @@ import React from "react";
 
 const Menu = () => {
   return (
-    <div className="menuContainer">
-      <p>MENU TEST</p>
-    </div>
+    <>
+      <div className="menuContainer">
+        <h2>Menu</h2>
+        <h3>Outdoor Doors</h3>
+        <p>Complete sets</p>
+        <p>Patio table</p>
+        <p>Chairs</p>
+        <h3>Indoor Goods</h3>
+        <p>Tables</p>
+        <p>Chairs</p>
+        <p>Couches</p>
+        <h3>Accessories</h3>
+        <p>Cushions</p>
+        <p>Blankets</p>
+      </div>
+    </>
   );
 };
 
