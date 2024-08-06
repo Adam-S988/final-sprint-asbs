868cb9af8b0d2ff7dfc740423e8dc53c61a26c84
Brandon Shea <brandon.shea1994@icloud.com>
2024-08-06 13:36:07 -0230

    prices

--- src/Components/api.js
+++ src/Components/api.js
@@ -4,6 +4,17 @@
 const prodQuantity = 10;
 const prodPrice = 99.99;
 const prodPrice1 = 199.99;
+const prodPrice2 = 49.99;
+const prodPrice3 = 69.99;
+const prodPrice4 = 79.99;
+const prodPrice5 = 199.99;
+const prodPrice6 = 349.99;
+const prodPrice7 = 99.99;
+const prodPrice8 = 129.99;
+const prodPrice9 = 169.99;
+const prodPrice10 = 149.99;
+const prodPrice11 = 119.99;
+const prodPrice12 = 39.99;
 
 const products = [
   {
@@ -19,7 +30,7 @@ const products = [
     name: "Naeemaroe Reclining Chair",
     description: "Placeholder Description 2",
 
-    price: prodPrice,
+    price: prodPrice2,
     Image:
       "https://www.ikea.com/ca/en/images/products/naemmaroe-reclining-chair-outdoor-foldable-light-brown-stained__1138757_pe880057_s5.jpg?f=xs",
   },
@@ -28,7 +39,7 @@ const products = [
     name: "Naeemaroe Seat Section",
     description: "Placeholder Description 3",
 
-    price: prodPrice,
+    price: prodPrice3,
     Image:
       "https://www.ikea.com/ca/en/images/products/naemmaroe-seat-sec-for-modular-sofa-outdoor-light-brown-stained__1138748_pe880048_s5.jpg?f=xs",
   },
@@ -37,7 +48,7 @@ const products = [
     name: "Naeemaroe Stool",
     description: "Placeholder Description 4",
 
-    price: prodPrice,
+    price: prodPrice4,
     Image:
       "https://www.ikea.com/ca/en/images/products/naemmaroe-stool-outdoor-foldable-light-brown-stained__1138761_pe880061_s5.jpg?f=xs",
   },
@@ -46,7 +57,7 @@ const products = [
     name: "Bondholmen Loveseat",
     description: "Placeholder Description 5",
 
-    price: prodPrice,
+    price: prodPrice5,
     Image:
       "https://www.ikea.com/ca/en/images/products/bondholmen-loveseat-outdoor-brown-froesoen-duvholmen-dark-gray__1247781_pe922788_s5.jpg?f=xs",
   },
@@ -55,7 +66,7 @@ const products = [
     name: "Tväröe Set",
     description: "Placeholder Description 6",
 
-    price: prodPrice,
+    price: prodPrice6,
     Image:
       "https://www.ikea.com/ca/en/images/products/tvaroe-froesoen-4-seat-conversation-set-outdoor-dark-gray__1206716_pe907580_s5.jpg?f=xs",
   },
@@ -64,7 +75,7 @@ const products = [
     name: "Solleoen Seat Section",
     description: "Placeholder Description 7",
 
-    price: prodPrice,
+    price: prodPrice7,
     Image:
       "https://www.ikea.com/ca/en/images/products/solleroen-seat-section-for-modular-sofa-outdoor-dark-gray-jaerpoen-duvholmen-anthracite__0991458_pe819463_s5.jpg?f=xs",
   },
@@ -73,7 +84,7 @@ const products = [
     name: "Solleroen Corner Section",
     description: "Placeholder Description 8",
 
-    price: prodPrice,
+    price: prodPrice8,
     Image:
       "https://www.ikea.com/ca/en/images/products/solleroen-corner-section-outdoor-brown__0729492_pe737011_s5.jpg?f=xs",
   },
@@ -82,7 +93,7 @@ const products = [
     name: "Solleroen 3-Seat Modular Sofa",
     description: "Placeholder Description 9",
 
-    price: prodPrice,
+    price: prodPrice9,
     Image:
       "https://www.ikea.com/ca/en/images/products/solleroen-3-seat-modular-sofa-outdoor-dark-gray-kuddarna-gray__0727777_pe735858_s5.jpg?f=xs",
   },
@@ -91,7 +102,7 @@ const products = [
     name: "Jolpen Loveseat",
     description: "Placeholder Description 10",
 
-    price: prodPrice,
+    price: prodPrice10,
     Image:
       "https://www.ikea.com/ca/en/images/products/jolpen-loveseat-outdoor-white-gray-kuddarna-beige__1146146_pe882916_s5.jpg?f=xs",
   },
@@ -100,7 +111,7 @@ const products = [
     name: "Solleroen Armchair",
     description: "Placeholder Description 11",
 
-    price: prodPrice,
+    price: prodPrice11,
     Image:
       "https://www.ikea.com/ca/en/images/products/solleroen-armchair-outdoor-brown-froesoen-duvholmen-dark-gray__0589814_pe673479_s5.jpg?f=xs",
   },
@@ -109,7 +120,7 @@ const products = [
     name: "Torparoe Armchair",
     description: "Placeholder Description 12",
 
-    price: prodPrice,
+    price: prodPrice12,
     Image:
       "https://www.ikea.com/ca/en/images/products/torparoe-armchair-in-outdoor-white-gray__1138957_pe880162_s5.jpg?f=xs",
   },
