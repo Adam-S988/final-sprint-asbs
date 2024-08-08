a5fc59f6b1f013b9e49d992ffe1561b8bce994c1
Brandon Shea <brandon.shea1994@icloud.com>
2024-08-08 12:38:59 -0230

    init cart

---
+++ src/Components/Cart.jsx
@@ -0,0 +1,17 @@
+import { getProducts } from "./api";
+import React from "react";
+import Header from "./Header";
+import Footer from "./Footer";
+
+const Cart = () => {
+  <>
+    return{" "}
+    <div>
+      <Header />
+      <Footer />
+    </div>
+    ;
+  </>;
+};
+
+export default Cart;
